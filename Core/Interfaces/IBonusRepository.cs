6856e176de9605ea55dc4906109947e125a7a057
Atul Kumar <atul80100@gmail.com>
2021-12-22 17:42:49 +0530

    First Commit

---
+++ Core/Interfaces/IBonusRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IBonusRepository
+    {
+        List<Employee> GetEmployeeWithCompany(int id);
+
+        Company GetCompanyWithEmployees(int id);
+
+        List<Company> GetAllCompanyWithEmployees();
+
+        void AddTestCompanyWithEmployees(Company objComp);
+
+        void AddTestCompanyWithEmployeesWithTransaction(Company objComp);
+
+        void RemoveRange(int[] companyId);
+
+        List<Company> FilterCompanyByName(string name);
+    }
+}
+ 
---
+++ Core/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+
+        Company Add(Company company);
+        Company Update(Company company);
+
+        void Remove(int id);
+
+    }
+}
